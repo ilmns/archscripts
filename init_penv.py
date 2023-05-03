8df64d140ac80caf1a28d823663b1afcb136f586
ilmns <33120809+ilmns@users.noreply.github.com>
2023-05-03 22:55:14 +0300

    Update init_penv.py

--- init_penv.py
+++ init_penv.py
@@ -1,9 +1,59 @@
 #!/usr/bin/env python3
 
+
 import os
+import shutil
 import subprocess
 import sys
+import platform
+import logging
 from pathlib import Path
+from rich import print
+from rich.console import Console
+from rich.table import Table
+
+
+def setup_logging():
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.INFO)
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+
+
+
+def delete_virtual_environment(env_path):
+    shutil.rmtree(env_path)
+    logging.info(f"Deleted virtual environment at {env_path}")
+    print(f"Deleted virtual environment at {env_path}")
+
+
+def clone_virtual_environment(src_env_path, dest_env_name, folder_path):
+    dest_env_path = os.path.join(folder_path, dest_env_name)
+    shutil.copytree(src_env_path, dest_env_path)
+    logging.info(f"Cloned virtual environment at {dest_env_path}")
+    print(f"Cloned virtual environment at {dest_env_path}")
+
+
+def update_package(env_path, package_name):
+    subprocess.run([os.path.join(env_path, "bin", "pip"), "install", "--upgrade", package_name], check=True)
+    logging.info(f"{package_name} updated successfully.")
+    print(f"{package_name} updated successfully.")
+
+
+def downgrade_package(env_path, package_name, version):
+    subprocess.run([os.path.join(env_path, "bin", "pip"), "install", f"{package_name}=={version}"], check=True)
+    logging.info(f"{package_name} downgraded to version {version}.")
+    print(f"{package_name} downgraded to version {version}.")
+
+
+def uninstall_package(env_path, package_name):
+    subprocess.run([os.path.join(env_path, "bin", "pip"), "uninstall", "-y", package_name], check=True)
+    logging.info(f"{package_name} uninstalled successfully.")
+    print(f"{package_name} uninstalled successfully.")
+
 
 def is_executable_installed(executable):
     try:
@@ -12,20 +62,45 @@ def is_executable_installed(executable):
     except FileNotFoundError:
         return False
 
+
 def search_existing_envs(folder_path):
     envs = [d for d in os.listdir(folder_path) if os.path.isdir(os.path.join(folder_path, d))]
     return envs
 
+
+def create_virtual_environment(env_path):
+    python_executable = "python3" if platform.system() != "Windows" else "python"
+    subprocess.run([python_executable, "-m", "venv", env_path], check=True)
+    logging.info(f"Created virtual environment at {env_path}")
+    print(f"Created virtual environment at {env_path}")
+
+
 def create_requirements_file(env_path):
     subprocess.run([os.path.join(env_path, "bin", "pip"), "freeze", "--local", ">", "requirements.txt"], shell=True, check=True, text=True)
+    logging.info("requirements.txt file created successfully.")
+    print("requirements.txt file created successfully.")
+
 
 def upgrade_packages(env_path):
     subprocess.run([os.path.join(env_path, "bin", "pip"), "install", "--upgrade", "pip", "setuptools", "wheel"], check=True)
+    logging.info("pip, setuptools, and wheel upgraded successfully.")
+    print("pip, setuptools, and wheel upgraded successfully.")
 
 def list_installed_packages(env_path):
     result = subprocess.run([os.path.join(env_path, "bin", "pip"), "list"], capture_output=True, check=True, text=True)
-    print("\nInstalled packages in the selected environment:")
-    print(result.stdout)
+
+    console = Console()
+    table = Table(show_header=True, header_style="bold magenta")
+    table.add_column("Package")
+    table.add_column("Version")
+
+    lines = result.stdout.split("\n")[2:-1]  # Remove header lines and trailing newline
+    for line in lines:
+        package, version = line.split()
+        table.add_row(package, version)
+
+    console.print("\nInstalled packages in the selected environment:")
+    console.print(table)
 
 def create_requirements_file(env_path):
     subprocess.run([os.path.join(env_path, "bin", "pip"), "freeze", "--local", ">", "requirements.txt"], shell=True, check=True)
@@ -86,12 +161,81 @@ def main():
     print("\nDone! You can now install packages using pip inside your virtual environment.")
 
     return env_path  # Add this line to the end of main()
+
+
+def search_and_install_packages(env_path):
+    try:
+        subprocess.run([os.path.join(env_path, "bin", "pip"), "install", "pip_search"], check=True)
+    except subprocess.CalledProcessError:
+        print("Failed to install pip_search. Skipping package search and installation.")
+        return
+
+    while True:
+        search_term = input("Enter the package name to search or 'q' to quit: ").strip()
+        if search_term.lower() == 'q':
+            break
+
+        search_command = f"{os.path.join(env_path, 'bin', 'python')} -m pip_search {search_term}"
+        result = subprocess.run(search_command, capture_output=True, check=True, text=True, shell=True)
+        print(f"\nSearch results for '{search_term}':\n{result.stdout}")
+
+        package_to_install = input("Enter the package name to install or 'q' to quit: ").strip()
+        if package_to_install.lower() == 'q':
+            break
+        subprocess.run([os.path.join(env_path, "bin", "pip"), "install", package_to_install], check=True)
+        print(f"\n{package_to_install} installed successfully.")
+
+
+# Modify main_loop function
+def main_loop(env_path):
+    while True:
+        action = prompt_for_action()
+
+        if action == '1':
+            list_installed_packages(env_path)
+        elif action == '2':
+            create_requirements_file(env_path)
+            print("requirements.txt file created successfully.")
+        elif action == '3':
+            search_and_install_packages(env_path)
+        elif action == '4':
+            delete_virtual_environment(env_path)
+            break
+        elif action == '5':
+            dest_env_name = prompt_user("Enter the name of the cloned virtual environment", "cloned_env")
+            clone_virtual_environment(env_path, dest_env_name, os.path.dirname(env_path))
+        elif action == '6':
+            package_name = input("Enter the package name to update: ").strip()
+            update_package(env_path, package_name)
+        elif action == '7':
+            package_name = input("Enter the package name to downgrade: ").strip()
+            version = input("Enter the version to downgrade to: ").strip()
+            downgrade_package(env_path, package_name, version)
+        elif action == '8':
+            package_name = input("Enter the package name to uninstall: ").strip()
+            uninstall_package(env_path, package_name)
+        elif action.lower() == 'q':
+            break
+        else:
+            print("Invalid input. Please try again.")
+
+# Modify prompt_for_action function
+def prompt_for_action():
+    console = Console()
+    console.print("\nChoose an action:", style="bold")
+    console.print("1. List installed packages")
+    console.print("2. Create requirements.txt file")
+    console.print("3. Search and install pip packages")
+    console.print("4. Delete virtual environment")
+    console.print("5. Clone virtual environment")
+    console.print("6. Update a package")
+    console.print("7. Downgrade a package")
+    console.print("8. Uninstall a package")
+    console.print("q. Quit")
+
+    user_input = input("Enter the number of the action or 'q' to quit: ").strip()
+    return user_input
+
 if __name__ == "__main__":
     env_path = main()
-
-    if yes_no_prompt("\nDo you want to see the list of installed packages in the selected environment?"):
-        list_installed_packages(env_path)
-
-    if yes_no_prompt("\nDo you want to create a requirements.txt file for the installed packages in the selected environment?"):
-        create_requirements_file(env_path)
-        print("requirements.txt file created successfully.")
+    main_loop(env_path)
