d9649a7cb2840a084b19a1767b39a49f9c16dbda
ilmns <33120809+ilmns@users.noreply.github.com>
2023-06-22 05:12:23 +0300

    Update maintenance.py

--- maintenance.py
+++ maintenance.py
@@ -5,11 +5,63 @@ import random
 def run_command(command):
     subprocess.run(command, check=True)
 
+def system_administration_tool():
+    print("System Administration Tool")
+    print("==========================")
+    print("1. Update system")
+    print("2. Remove unnecessary packages")
+    print("3. Clean package cache")
+    print("4. Configure automatic updates")
+    print("5. Backup important files")
+    print("6. Monitor system logs")
+    print("7. Check security updates")
+    print("8. Perform system cleanup")
+    print("9. Display system information")
+    print("10. Surprise upgrades")
+    print("11. Install additional packages")
+    print("12. Change wallpaper")
+    print("13. Take a screenshot")
+    print("14. Show calendar")
+
+    choice = input("Select an action (1-14): ")
+
+    if choice == "1":
+        update_system()
+    elif choice == "2":
+        remove_unnecessary_packages()
+    elif choice == "3":
+        clean_package_cache()
+    elif choice == "4":
+        configure_automatic_updates()
+    elif choice == "5":
+        backup_files()
+    elif choice == "6":
+        monitor_system_logs()
+    elif choice == "7":
+        check_security_updates()
+    elif choice == "8":
+        perform_system_cleanup()
+    elif choice == "9":
+        display_system_info()
+    elif choice == "10":
+        surprise_upgrades()
+    elif choice == "11":
+        install_additional_packages()
+    elif choice == "12":
+        change_wallpaper()
+    elif choice == "13":
+        take_screenshot()
+    elif choice == "14":
+        show_calendar()
+    else:
+        print("Invalid choice. Please select a valid option.")
+
 def update_system():
     run_command(['sudo', 'pacman', '-Syu', '--noconfirm'])
 
-def remove_packages(package_list):
-    run_command(['sudo', 'pacman', '-Rns'] + package_list)
+def remove_unnecessary_packages():
+    packages_to_remove = ['package1', 'package2', 'package3']
+    run_command(['sudo', 'pacman', '-Rns'] + packages_to_remove)
 
 def clean_package_cache():
     run_command(['sudo', 'paccache', '-r'])
@@ -28,14 +80,12 @@ def configure_automatic_updates(script_path='/path/to/update.sh'):
     # Add the cron entry
     run_command(['echo', '{} | crontab -'.format(cron_entry)], shell=True)
 
-def backup_files(source='/path/to/source/', destination='/path/to/backup/destination/'):
-    # Perform backup operations from the source to the destination
-    # Example: Use rsync to backup files to an external drive
-    run_command(['rsync', '-av', '--delete', source, destination])
+def backup_files():
+    source_directory = input('Enter the source directory: ') or '/path/to/source/'
+    backup_destination = '/path/to/backup/destination/'
+    run_command(['rsync', '-av', '--delete', source_directory, backup_destination])
 
 def monitor_system_logs():
-    # Monitor system logs and take necessary actions
-    # Example: Read log files and check for specific patterns or errors
     log_files = ['/var/log/syslog', '/var/log/messages']
     for log_file in log_files:
         with open(log_file, 'r') as file:
@@ -54,74 +104,26 @@ def display_system_info():
     run_command(['neofetch'])
 
 def surprise_upgrades():
-    # Add surprise upgrades or features
     upgrade_options = ['Upgrade 1', 'Upgrade 2', 'Upgrade 3']
     random_upgrade = random.choice(upgrade_options)
     print('Surprise Upgrade: {}'.format(random_upgrade))
 
-def install_additional_packages(package_list):
-    run_command(['sudo', 'pacman', '-S', '--noconfirm'] + package_list)
+def install_additional_packages():
+    additional_packages = ['package4', 'package5']
+    run_command(['sudo', 'pacman', '-S', '--noconfirm'] + additional_packages)
 
-def change_wallpaper(wallpaper_path):
+def change_wallpaper():
+    wallpaper_path = '/path/to/wallpaper.png'
     run_command(['feh', '--bg-scale', wallpaper_path])
 
-def take_screenshot(save_directory='/path/to/save/directory/'):
+def take_screenshot():
+    screenshot_save_directory = '/path/to/save/directory/'
     screenshot_name = 'screenshot.png'
-    screenshot_path = os.path.join(save_directory, screenshot_name)
+    screenshot_path = os.path.join(screenshot_save_directory, screenshot_name)
     run_command(['scrot', screenshot_path])
 
 def show_calendar():
     run_command(['cal'])
 
-def main():
-    # Update the system
-    update_system()
-
-    # Remove unnecessary packages
-    packages_to_remove = ['package1', 'package2', 'package3']
-    remove_packages(packages_to_remove)
-
-    # Clean package cache
-    clean_package_cache()
-
-    # Configure automatic updates
-    script_path = '/path/to/update.sh'
-    configure_automatic_updates(script_path)
-
-    # Backup important files
-    source_directory = input('Enter the source directory: ') or '/path/to/source/'
-    backup_destination = '/path/to/backup/destination/'
-    backup_files(source_directory, backup_destination)
-
-    # Monitor system logs
-    monitor_system_logs()
-
-    # Check for security updates
-    check_security_updates()
-
-    # Perform system cleanup
-    perform_system_cleanup()
-
-    # Display system information
-    display_system_info()
-
-    # Surprise upgrades
-    surprise_upgrades()
-
-    # Install additional packages
-    additional_packages = ['package4', 'package5']
-    install_additional_packages(additional_packages)
-
-    # Change wallpaper
-    wallpaper_path = '/path/to/wallpaper.png'
-    change_wallpaper(wallpaper_path)
-
-    # Take a screenshot
-    screenshot_save_directory = '/path/to/save/directory/'
-    take_screenshot(screenshot_save_directory)
-
-    # Show calendar
-    show_calendar()
-
-if __name__ == '__main__':
-    main()
+# Main program
+system_administration_tool()
