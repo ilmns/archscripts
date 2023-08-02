355067ffab74ca7f95a8bf0aad527bce090cd792
ilmns <33120809+ilmns@users.noreply.github.com>
2023-08-03 00:45:45 +0300

    Update bspwm_menu.py

--- bspwm_menu.py
+++ bspwm_menu.py
@@ -8,7 +8,7 @@ config_dir = home_dir / ".config"
 bspwm_dir = config_dir / "bspwm"
 sxhkd_dir = config_dir / "sxhkd"
 polybar_dir = config_dir / "polybar"
-lightdm_config_dir = Path("/etc/lightdm")
+lightdm_config_dir = config_dir / "lightdm"
 accountservice_dir = Path("/var/lib/AccountsService/users")
 
 bspwm_config_file = bspwm_dir / "bspwmrc"
@@ -32,14 +32,18 @@ def ask_sudo_password():
     return sudo_password
 
 
-def install_packages(sudo_password):
+def run_command(command):
     try:
-        packages = ["bspwm", "sxhkd", "polybar", "lightdm", "lightdm-gtk-greeter"]
-        subprocess.run(['echo', sudo_password, '|', 'sudo', '-S', 'pacman', '-S', '--noconfirm'] + packages, check=True)
+        subprocess.run(command, check=True)
     except subprocess.CalledProcessError as e:
-        print("An error occurred during package installation:")
+        print(f"Error occurred while running command: {command}")
         print(e)
-        exit(1)
+
+
+def install_packages(sudo_password):
+    packages = ["bspwm", "sxhkd", "polybar", "lightdm", "lightdm-gtk-greeter"]
+    command = f"echo {sudo_password} | sudo -S pacman -S --noconfirm {' '.join(packages)}"
+    run_command(command)
 
 
 def setup_config_files():
@@ -97,20 +101,35 @@ def setup_config_files():
     """
 
     # Write config files
-    with open(bspwm_config_file, 'w') as file:
-        file.write(bspwm_config)
+    try:
+        with open(bspwm_config_file, 'w') as file:
+            file.write(bspwm_config)
+    except Exception as e:
+        print(f"Error writing bspwm config: {e}")
 
-    with open(sxhkd_config_file, 'w') as file:
-        file.write(sxhkd_config)
+    try:
+        with open(sxhkd_config_file, 'w') as file:
+            file.write(sxhkd_config)
+    except Exception as e:
+        print(f"Error writing sxhkd config: {e}")
 
-    with open(polybar_config_file, 'w') as file:
-        file.write(polybar_config)
+    try:
+        with open(polybar_config_file, 'w') as file:
+            file.write(polybar_config)
+    except Exception as e:
+        print(f"Error writing polybar config: {e}")
 
-    with open(lightdm_gtk_greeter_config_file, 'w') as file:
-        file.write(lightdm_gtk_greeter_config)
+    try:
+        with open(lightdm_gtk_greeter_config_file, 'w') as file:
+            file.write(lightdm_gtk_greeter_config)
+    except Exception as e:
+        print(f"Error writing lightdm-gtk-greeter config: {e}")
 
-    with open(accountservice_file, 'w') as file:
-        file.write(accountservice_config)
+    try:
+        with open(accountservice_file, 'w') as file:
+            file.write(accountservice_config)
+    except Exception as e:
+        print(f"Error writing accountservice config: {e}")
 
 
 def enable_at_start():
@@ -131,10 +150,11 @@ def enable_at_start():
     """
     bspwm_desktop_file.parent.mkdir(parents=True, exist_ok=True)
 
-    with open(bspwm_desktop_file, 'w') as file:
-        file.write(desktop_file_content)
-
-    subprocess.run(['sudo', 'systemctl', 'enable', 'lightdm.service'], check=True)
+    try:
+        with open(bspwm_desktop_file, 'w') as file:
+            file.write(desktop_file_content)
+    except Exception as e:
+        print(f"Error writing bspwm.desktop file: {e}")
 
 
 def main():
